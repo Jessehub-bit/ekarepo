195b7806f20c5c834599bed50fba31a2b3a34937
Jesse Hotarinen <69570552+Jessehub-bit@users.noreply.github.com>
2021-08-17 20:26:36 +0300

    Add files via upload

---
+++ Harjoitus2/Harjoitus2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Harjoitus2
+{
+    public partial class Harjoitus2 : Form
+    {
+        public Harjoitus2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViestiTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TulostaBT_Click(object sender, EventArgs e)
+        {
+            string teksti = ViestiTB.Text;
+
+            TulostusLB.Text = teksti;
+
+            TulostusLB.Visible = true;
+        }
+    }
+}
