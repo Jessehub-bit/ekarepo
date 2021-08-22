72151efc656bc611e915177b6373870ec06a8f8e
Jesse Hotarinen <69570552+Jessehub-bit@users.noreply.github.com>
2021-08-22 15:49:05 +0300

    Add files via upload

---
+++ Harjoitus_5/Harjoitus_5/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Harjoitus_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.uusilukuTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anna luku (-999 lopetus):";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Location = new System.Drawing.Point(12, 33);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(38, 15);
+            this.VastausLB.TabIndex = 1;
+            this.VastausLB.Text = "label2";
+            this.VastausLB.Visible = false;
+            // 
+            // uusilukuTB
+            // 
+            this.uusilukuTB.Location = new System.Drawing.Point(158, 6);
+            this.uusilukuTB.Name = "uusilukuTB";
+            this.uusilukuTB.Size = new System.Drawing.Size(100, 23);
+            this.uusilukuTB.TabIndex = 2;
+            this.uusilukuTB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.uusilukuTB_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 59);
+            this.Controls.Add(this.uusilukuTB);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.TextBox uusilukuTB;
+    }
+}
+
