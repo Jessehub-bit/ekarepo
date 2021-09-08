9e17b64e6109c6207e4e90f2ac57c74c26a722fc
Jesse Hotarinen <69570552+Jessehub-bit@users.noreply.github.com>
2021-09-09 00:10:14 +0300

    Add files via upload

---
+++ 17. Tehtävä - Muistio/17. Tehtävä - Muistio/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace _17._Tehtävä___Muistio
+{
+    partial class NotepadForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uusiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tallennaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lopetaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.muoitoileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kirjaisinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 426);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiedostoToolStripMenuItem,
+            this.muoitoileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            this.tiedostoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uusiToolStripMenuItem,
+            this.avaaToolStripMenuItem,
+            this.tallennaToolStripMenuItem,
+            this.lopetaToolStripMenuItem});
+            this.tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            this.tiedostoToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.tiedostoToolStripMenuItem.Text = "Tiedosto";
+            // 
+            // uusiToolStripMenuItem
+            // 
+            this.uusiToolStripMenuItem.Name = "uusiToolStripMenuItem";
+            this.uusiToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.uusiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.uusiToolStripMenuItem.Text = "Uusi";
+            this.uusiToolStripMenuItem.Click += new System.EventHandler(this.uusiToolStripMenuItem_Click);
+            // 
+            // avaaToolStripMenuItem
+            // 
+            this.avaaToolStripMenuItem.Name = "avaaToolStripMenuItem";
+            this.avaaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.avaaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.avaaToolStripMenuItem.Text = "Avaa";
+            this.avaaToolStripMenuItem.Click += new System.EventHandler(this.avaaToolStripMenuItem_Click);
+            // 
+            // tallennaToolStripMenuItem
+            // 
+            this.tallennaToolStripMenuItem.Name = "tallennaToolStripMenuItem";
+            this.tallennaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.tallennaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tallennaToolStripMenuItem.Text = "Tallenna";
+            this.tallennaToolStripMenuItem.Click += new System.EventHandler(this.tallennaToolStripMenuItem_Click);
+            // 
+            // lopetaToolStripMenuItem
+            // 
+            this.lopetaToolStripMenuItem.Name = "lopetaToolStripMenuItem";
+            this.lopetaToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.lopetaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lopetaToolStripMenuItem.Text = "Lopeta";
+            this.lopetaToolStripMenuItem.Click += new System.EventHandler(this.lopetaToolStripMenuItem_Click);
+            // 
+            // muoitoileToolStripMenuItem
+            // 
+            this.muoitoileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kirjaisinToolStripMenuItem});
+            this.muoitoileToolStripMenuItem.Name = "muoitoileToolStripMenuItem";
+            this.muoitoileToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.muoitoileToolStripMenuItem.Text = "Muotoile";
+            // 
+            // kirjaisinToolStripMenuItem
+            // 
+            this.kirjaisinToolStripMenuItem.Name = "kirjaisinToolStripMenuItem";
+            this.kirjaisinToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.F)));
+            this.kirjaisinToolStripMenuItem.Size = new System.Drawing.Size(187, 22);
+            this.kirjaisinToolStripMenuItem.Text = "Kirjaisin";
+            this.kirjaisinToolStripMenuItem.Click += new System.EventHandler(this.kirjaisinToolStripMenuItem_Click);
+            // 
+            // NotepadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "NotepadForm";
+            this.Text = "Pieni muistio";
+            this.Load += new System.EventHandler(this.NotepadForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tiedostoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uusiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem avaaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tallennaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lopetaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem muoitoileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kirjaisinToolStripMenuItem;
+    }
+}
+
---
+++ 17. Tehtävä - Muistio/17. Tehtävä - Muistio/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _17._Tehtävä___Muistio
+{
+    public partial class NotepadForm : Form
+    {
+        private OpenFileDialog OpenFileDialog;
+        private SaveFileDialog saveFileDialog;
+        private FontDialog fontDialog;
+        public NotepadForm()
+        {
+            InitializeComponent();
+        }
+
+        private void UusiTiedosto()
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(richTextBox1.Text))
+                {
+                    MessageBox.Show("Sinun tulee tallentaa ensin!");
+                }
+                else
+                {
+                    richTextBox1.Text = string.Empty;
+                    Text = "Nimetön";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+            
+        }
+
+        private void TallennaTiedosto()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(richTextBox1.Text))
+                {
+                    saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "Tekstitiedosto (*.txt) | *.txt|Rikas tekstiformaatti (*.rtf) | *.rtf";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, richTextBox1.Text);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+
+        private void AvaaTiedosto()
+        {
+            try
+            {
+                OpenFileDialog = new OpenFileDialog();
+                if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    richTextBox1.Text = File.ReadAllText(OpenFileDialog.FileName);
+                    Text = OpenFileDialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe avataessa tiedostoa: " + ex);
+            }
+        }
+
+
+       
+        private void NotepadForm_Load(object sender, EventArgs e)
+        {
+            fontDialog = new FontDialog();
+        }
+
+        private void uusiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UusiTiedosto();
+        }
+
+        private void avaaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AvaaTiedosto();
+        }
+
+        private void tallennaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TallennaTiedosto();
+        }
+
+        private void lopetaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(richTextBox1.Text))
+                {
+                    TallennaTiedosto();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+
+        private void kirjaisinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(fontDialog.ShowDialog() == DialogResult.OK)
+                {
+                    richTextBox1.Font = fontDialog.Font;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Virhe: " + ex);
+            }
+        }
+    }
+}
