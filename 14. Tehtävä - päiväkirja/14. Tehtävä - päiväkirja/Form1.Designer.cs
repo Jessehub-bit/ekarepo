604e107352043c6422d1b2c2f53e545c5f9e8e4f
Jesse Hotarinen <69570552+Jessehub-bit@users.noreply.github.com>
2021-09-08 19:11:35 +0300

    Add files via upload

---
+++ 14. Tehtävä - päiväkirja/14. Tehtävä - päiväkirja/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace _14._Tehtävä___päiväkirja
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TallennaBT = new System.Windows.Forms.Button();
+            this.SyottoTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TallennaBT
+            // 
+            this.TallennaBT.Location = new System.Drawing.Point(12, 354);
+            this.TallennaBT.Name = "TallennaBT";
+            this.TallennaBT.Size = new System.Drawing.Size(734, 73);
+            this.TallennaBT.TabIndex = 0;
+            this.TallennaBT.Text = "Tallenna päiväkirja";
+            this.TallennaBT.UseVisualStyleBackColor = true;
+            this.TallennaBT.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SyottoTB
+            // 
+            this.SyottoTB.Location = new System.Drawing.Point(12, 0);
+            this.SyottoTB.Multiline = true;
+            this.SyottoTB.Name = "SyottoTB";
+            this.SyottoTB.Size = new System.Drawing.Size(734, 348);
+            this.SyottoTB.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 430);
+            this.Controls.Add(this.SyottoTB);
+            this.Controls.Add(this.TallennaBT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TallennaBT;
+        private System.Windows.Forms.TextBox SyottoTB;
+    }
+}
+
---
+++ 14. Tehtävä - päiväkirja/14. Tehtävä - päiväkirja/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _14._Tehtävä___päiväkirja
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string teksti = System.IO.File.ReadAllText(@"C:\\Users\\Hotsku\\source\\repos\\14. Tehtävä - päiväkirja\\demo.txt");
+            teksti += SyottoTB.Text;
+            teksti += " " + DateTime.Now.ToString("dd.MM.yyyy HH:mm") + "\n";
+            TextWriter txt = new StreamWriter("C:\\Users\\Hotsku\\source\\repos\\14. Tehtävä - päiväkirja\\demo.txt");
+            txt.Write(teksti);
+            txt.Close();
+            Application.Exit();
+        }
+    }
+}
